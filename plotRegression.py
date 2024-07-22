750dae6746f27dac0ff3b9238bbf7787b2068858
Nomel-Cool <619471786@qq.com>
2024-07-22 14:02:44 +0800

    修复了绘图窗口闪退现象

--- plotRegression.py
+++ plotRegression.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
-import time
 
 # 读取CSV文件中的参数
 with open('regression_params.csv', 'r') as f:
@@ -25,5 +24,5 @@ plt.xlim(left=0)  # x轴只显示非负实数区域
 plt.ylim(bottom=0)  # y轴只显示非负实数区域
 plt.show(block=False)  # 非阻塞显示
 
-time.sleep(1.5)  # 等待1.5秒
+plt.pause(3.5)  # 暂停2秒
 plt.close()  # 关闭显示窗口
