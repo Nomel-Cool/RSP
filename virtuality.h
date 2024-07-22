0ffc52dc1fee8b3dd7c44c01f370cfcc42b41a76
Nomel-Cool <619471786@qq.com>
2024-07-22 17:58:23 +0800

    修改了上分位置占比的计算意义为局部检测，目前reality的模型大致意义区间分布在0.2~0.4之间

--- virtuality.h
+++ virtuality.h
@@ -35,16 +35,36 @@ public:
 	{
 
 	}
-	virtual void interaction(size_t i, size_t j)
+
+	/// <summary>
+	/// 执行总体回归检测
+	/// </summary>
+	/// <param name="i"></param>
+	/// <param name="j"></param>
+	virtual void _interaction(size_t i, size_t j)
 	{
 		if (i + 1 > N || i + 1 <= 0 || j + 1 <= 0 || j + 1 > N)
 			throw;
-		double determined_position = getPosition(i + 1, j + 1) / SHRINK_RATIO;  // 缩小10^10倍
+		double determined_position = _getPosition(i + 1, j + 1) / SHRINK_RATIO;  // 缩小10^10倍
 		double grass = std::pow((N - 1) * N / 2, std::get<0>(m_up_pos)) / SHRINK_RATIO;  // 缩小10^10倍
 		double positon_ratio = static_cast<double>(determined_position) / grass;
 		m_current_ratio = positon_ratio;
 	}
 
+	/// <summary>
+	/// 执行局部回归检测
+	/// </summary>
+	/// <param name="i"></param>
+	/// <param name="j"></param>
+	virtual void interaction(size_t i, size_t j)
+	{
+		if (i + 1 > N || i + 1 <= 0 || j + 1 <= 0 || j + 1 > N)
+			throw;
+		double relative_position = getPosition(i + 1, j + 1);
+		double grass = (N - 1) * N / 2;
+		double positon_ratio = static_cast<double>(relative_position) / grass;
+		m_current_ratio = positon_ratio;
+	}
 
 	virtual float getRatio()
 	{
@@ -62,7 +82,7 @@ public:
 	}
 
 protected:
-	virtual size_t getPosition(size_t i, size_t j)
+	virtual size_t _getPosition(size_t i, size_t j)
 	{
 		if (j < i)
 			std::swap(i, j);
@@ -72,6 +92,16 @@ protected:
 		m_up_pos = std::make_tuple(std::get<0>(m_up_pos) + 1, determined_position);
 		return determined_position;
 	}
+
+	virtual size_t getPosition(size_t i, size_t j)
+	{
+		if (j < i)
+			std::swap(i, j);
+		size_t relative_position = (((2 * N - i) * (i - 1)) >> 1) + j - i;
+		m_up_pos = std::make_tuple(std::get<0>(m_up_pos) + 1, relative_position);
+		return relative_position;
+	}
+
 	size_t C(size_t n, size_t m)
 	{
 		size_t ans = 1;
