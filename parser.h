b6ec448a36e4aa1c2484f37e44ae96f4859432ff
Nomel-Cool <619471786@qq.com>
2024-08-06 13:28:40 +0800

    1.重载了Interaction函数使能控制交互深度或扩张程度；2.将追加实验函数中的交互代码由已封装的Interaction代替，同时将抽象形成序的分析任务分配到classify函数

--- parser.h
+++ parser.h
@@ -24,30 +24,33 @@ public:
 	{
 
 	}
-	void Interaction(
+	SizeT7 Interaction(
 		const std::string& manipulate_node,
 		const std::string& accuracy_file,
 		const std::string& answer_file,
 		const std::string& order_file,
 		const std::string& ratio_file,
 		const std::string& output_file,
-		const std::string& regression_file
+		const std::string& regression_file,
+		const bool isStored = false,
+		size_t interaction_depth = 1 // 交互深度
 	)
 	{
-		while (m_append_test_count--)
+		SizeT7 feed_back;
+		while (interaction_depth--)
 		{
 			//统计交互参数，返回上层
 			m_dispatch.statisticConvergence(manipulate_node, accuracy_file);
 			m_dispatch.statisticAnswering(manipulate_node, answer_file);
 
 			// 考察整体平衡度
-			auto feed_back = m_decise.gainFeedBack(accuracy_file, answer_file, order_file, ratio_file, regression_file);
+			feed_back = m_decise.gainFeedBack(accuracy_file, answer_file, order_file, ratio_file, regression_file);
 
 			// 接着制作交互控制
-			m_decise.makeOrders(feed_back, manipulate_node);
+			m_decise.makeOrders(feed_back, manipulate_node, order_file);
 
 			// 读取交互指令，实现交互
-			m_dispatch.interaction();
+			m_dispatch.interaction(isStored, order_file);
 
 			// 生成位置占比数据
 			m_dispatch.statisticRatio(manipulate_node, ratio_file);
@@ -59,11 +62,52 @@ public:
 			//system("pause");
 
 			// 自动迭代
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			//std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		}
-		m_append_test_count = 20;
+		return feed_back; // 返回最后一次交互后的反馈
 	}
-	std::tuple<std::vector<std::pair<size_t, size_t>>, std::vector<size_t>> ExamModel(
+	SizeT7 Interaction(
+		const std::string& manipulate_node,
+		const std::string& accuracy_file,
+		const std::string& answer_file,
+		const std::string& order_file,
+		const std::string& ratio_file,
+		const std::string& output_file,
+		const std::string& regression_file,
+		const size_t specify_i,
+		const size_t specify_j,
+		size_t expand_extension, // 扩张程度
+		const bool isStored = false,
+		size_t interaction_depth = 1 // 交互深度
+	)
+	{
+		SizeT7 feed_back;
+		while (interaction_depth--)
+		{
+			// 考察整体平衡度
+			feed_back = m_decise.gainFeedBack(accuracy_file, answer_file, order_file, ratio_file, regression_file, expand_extension, specify_i, specify_j);
+
+			// 接着制作交互控制
+			m_decise.makeOrders(feed_back, manipulate_node, order_file);
+
+			// 读取交互指令，实现交互
+			m_dispatch.interaction(isStored, order_file);
+
+			// 生成位置占比数据
+			m_dispatch.statisticRatio(manipulate_node, ratio_file);
+
+			// 展示交互结果
+			m_dispatch.show(manipulate_node, output_file);
+
+			//手动迭代
+			//system("pause");
+
+			// 自动迭代
+			//std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		}
+		return feed_back; // 返回最后一次交互后的反馈
+	}
+	std::vector<std::pair<size_t, size_t>> ExamModel(
 		const std::string& manipulate_node,
 		const std::string& exam_accuracy_file,
 		const std::string& exam_answer_file,
@@ -75,29 +119,32 @@ public:
 		const size_t& convergence_limit = 5
 	)
 	{
-		// 初始化实验环境
+		// 挂起现场
 		m_dispatch.preserver(manipulate_node);
+		//初始化实验环境
 		m_dispatch.examination(max_size, max_value, extra_size); // max_size, max_value, extra_size
 
 		std::tuple<size_t, size_t> first_interactor = std::make_tuple(0, 0);
-		std::vector<size_t> recur_w;
 		int counter = 0;
 		while (true)
 		{
-			m_dispatch.statisticConvergence(manipulate_node, extra_size, exam_accuracy_file);
-			m_dispatch.statisticAnswering(manipulate_node, extra_size, exam_answer_file);
-			auto feed_back = m_decise.gainFeedBack(exam_accuracy_file, exam_answer_file, exam_order_file, exam_ratio_file, exam_regression_file);
+			auto feed_back = Interaction(
+				manipulate_node,
+				exam_accuracy_file,
+				exam_answer_file,
+				exam_order_file,
+				exam_ratio_file,
+				exam_output_file,
+				exam_regression_file,
+				true
+			);
+
 			if (std::get<0>(first_interactor) == std::get<1>(first_interactor))
 				first_interactor = std::make_tuple(std::get<1>(feed_back), std::get<2>(feed_back));
+
 			if ((std::get<0>(first_interactor) == std::get<1>(feed_back) && std::get<1>(first_interactor) == std::get<2>(feed_back)) ||
 				(std::get<0>(first_interactor) == std::get<2>(feed_back) && std::get<1>(first_interactor) == std::get<1>(feed_back)))
 				counter++;
-			m_decise.makeOrders(feed_back, manipulate_node, exam_order_file);
-			m_dispatch.interaction(true, exam_order_file, extra_size); // 启动record模式
-			m_dispatch.statisticRatio(manipulate_node, exam_ratio_file);
-			m_dispatch.show(manipulate_node, exam_output_file);
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 自动迭代
 
 			if (counter == convergence_limit)
 			{
@@ -108,6 +155,24 @@ public:
 		// 到达认知收敛平衡标准后，获取virtuality记录的抽象形成序
 		auto sequences = m_dispatch.getInteractions(manipulate_node);
 
+		// 清楚抽象形成序记录缓存
+		m_dispatch.cleanCache(manipulate_node);
+
+		// 还原现场
+		m_dispatch.recover(manipulate_node);
+		return sequences;
+	}
+	std::map<std::pair<size_t, size_t>, std::vector<std::pair<size_t, size_t>>> classify(
+		const std::vector<std::pair<size_t, size_t>>& sequences,
+		const std::string& manipulate_node,
+		const std::string& exam_accuracy_file,
+		const std::string& exam_answer_file,
+		const std::string& exam_order_file,
+		const std::string& exam_ratio_file,
+		const std::string& exam_output_file,
+		const std::string& exam_regression_file)
+	{
+		std::vector<size_t> recur_w;
 		// 利用模型的收敛倾向，执行抽象形成序
 		for (size_t m = 2; m < sequences.size(); ++m)
 		{
@@ -116,10 +181,17 @@ public:
 			{
 				size_t exam_i = sequences[i].first;
 				size_t exam_j = sequences[i].second;
-				auto feed_back = m_decise.gainFeedBack(exam_accuracy_file, exam_answer_file, exam_order_file, exam_ratio_file, exam_regression_file, 1, exam_i, exam_j);
-				m_decise.makeOrders(feed_back, manipulate_node, exam_order_file);
-				m_dispatch.interaction(false, exam_order_file, 1); // 取消启动record模式
-				m_dispatch.show(manipulate_node, exam_output_file);
+				auto feed_back = Interaction(
+					manipulate_node,
+					exam_accuracy_file,
+					exam_answer_file,
+					exam_order_file,
+					exam_ratio_file,
+					exam_output_file,
+					exam_regression_file,
+					exam_i,
+					exam_j
+				);
 				std::cout << "<" << exam_i << "," << exam_j << ">" << "  |  ";
 			}
 			std::cout << '\n';
@@ -134,15 +206,8 @@ public:
 			}
 			m_dispatch.recover(manipulate_node);
 		}
-		m_dispatch.recover(manipulate_node);
-		return std::make_tuple(sequences, recur_w);
-	}
-	std::map<std::pair<size_t, size_t>, std::vector<std::pair<size_t, size_t>>> classify(std::tuple<std::vector<std::pair<size_t, size_t>>, std::vector<size_t>> raw_datas)
-	{
 		std::map<std::pair<size_t, size_t>, std::vector<std::pair<size_t, size_t>>> classified_syntax_datas;
-		auto sequences = std::get<0>(raw_datas);
-		auto stable_indices = std::get<1>(raw_datas);
-		for (const auto& i : stable_indices)
+		for (const auto& i : recur_w)
 		{
 			// 使用std::vector的insert函数插入一个范围
 			classified_syntax_datas[sequences[i]].insert(
@@ -154,7 +219,6 @@ public:
 	}
 
 protected:
-	size_t m_append_test_count = 20;
 private:
 	decision<N> m_decise;
 	dispatcher<N, max_size, max_value> m_dispatch;
