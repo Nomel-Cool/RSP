ec57984c52d58d0925a7e6afec7777f8fff0a520
Nomel-Cool <619471786@qq.com>
2024-08-07 18:41:09 +0800

    修正增函数会放入重复Key对的错误

--- dblink.h
+++ dblink.h
@@ -42,12 +42,33 @@ public:
     void Add(StableSequencesData datas)
     {
         for (const auto& [key, vectors] : datas) {
-            std::string insertMainMap = "INSERT INTO MainMap (key1, key2) VALUES (" + std::to_string(key.first) + ", " + std::to_string(key.second) + ")";
-            if (mysql_query(conn, insertMainMap.c_str())) {
-                throw std::runtime_error("INSERT INTO MainMap failed");
+            // 检查是否已经存在相同的键对
+            std::string checkMainMap = "SELECT id FROM MainMap WHERE (key1 = " + std::to_string(key.first) + " AND key2 = " + std::to_string(key.second) + ") OR (key1 = " + std::to_string(key.second) + " AND key2 = " + std::to_string(key.first) + ")";
+            if (mysql_query(conn, checkMainMap.c_str())) {
+                throw std::runtime_error("SELECT FROM MainMap failed");
             }
 
-            int mainMapId = mysql_insert_id(conn);
+            MYSQL_RES* res = mysql_store_result(conn);
+            if (res == nullptr) {
+                throw std::runtime_error("mysql_store_result() failed");
+            }
+
+            MYSQL_ROW row = mysql_fetch_row(res);
+            int mainMapId;
+            if (row != nullptr) {
+                // 键对已经存在，获取对应的 main_map_id
+                mainMapId = std::stoi(row[0]);
+                mysql_free_result(res);
+            }
+            else {
+                // 插入新的键对
+                mysql_free_result(res);
+                std::string insertMainMap = "INSERT INTO MainMap (key1, key2) VALUES (" + std::to_string(key.first) + ", " + std::to_string(key.second) + ")";
+                if (mysql_query(conn, insertMainMap.c_str())) {
+                    throw std::runtime_error("INSERT INTO MainMap failed");
+                }
+                mainMapId = mysql_insert_id(conn);
+            }
 
             for (size_t i = 0; i < vectors.size(); ++i) {
                 std::string insertVectorList = "INSERT INTO VectorList (main_map_id, vector_index) VALUES (" + std::to_string(mainMapId) + ", " + std::to_string(i) + ")";
