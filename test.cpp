3271a7088ddcf4d3b3f53446584c2e4b72c09e2b
Nomel-Cool <619471786@qq.com>
2024-08-07 16:00:49 +0800

    更新至使用封装追加实验算法并调用mysql库存储

--- test.cpp
+++ test.cpp
@@ -1,57 +1,59 @@
 #include "parser.h"
-
+#include "dblink.h"
 using namespace std;
 
 int main()
 {
-	std::string normal_node = "normal",
-		exam_node = "examing",
-		accuracy_file = "interaction_accuracy.csv",
-		answer_file = "interaction_answer.csv",
-		order_file = "interaction_orders.csv",
-		ratio_file = "interaction_ratio.csv",
-		output_file = "interaction_output.csv",
-		regression_file = "regression_params.csv",
-		exam_accuracy_file = "interaction_accuracy_exam.csv",
-		exam_answer_file = "interaction_answer_exam.csv",
-		exam_order_file = "interaction_orders_exam.csv",
-		exam_ratio_file = "interaction_ratio_exam.csv",
-		exam_output_file = "interaction_output_exam.csv",
-		exam_regression_file = "regression_params_exam.csv";
-	abstractParser<5, 12> parser_factory(5);
-	parser_factory.Interaction(
-		normal_node,
-		accuracy_file,
-		answer_file,
-		order_file,
-		ratio_file,
-		output_file,
-		regression_file,
-		false, // 不记录抽象形成序
-		20 // 交互深度为20
-	);
+    std::string normal_node = "normal",
+        exam_node = "examing",
+        accuracy_file = "interaction_accuracy.csv",
+        answer_file = "interaction_answer.csv",
+        order_file = "interaction_orders.csv",
+        ratio_file = "interaction_ratio.csv",
+        output_file = "interaction_output.csv",
+        regression_file = "regression_params.csv",
+        exam_accuracy_file = "interaction_accuracy_exam.csv",
+        exam_answer_file = "interaction_answer_exam.csv",
+        exam_order_file = "interaction_orders_exam.csv",
+        exam_ratio_file = "interaction_ratio_exam.csv",
+        exam_output_file = "interaction_output_exam.csv",
+        exam_regression_file = "regression_params_exam.csv";
+    abstractParser<5, 12> parser_factory(5);
+    dbManager db;
+    interaction_param normal_mode =
+    {
+        normal_node,
+        accuracy_file,
+        answer_file,
+        order_file,
+        ratio_file,
+        output_file,
+        regression_file,
+        false,
+        20, // 交互深度
+        0, // 交互规模
+        0,
+        0
+    };
+    interaction_param exam_mode =
+    {
+        exam_node,
+        exam_accuracy_file,
+        exam_answer_file,
+        exam_order_file,
+        exam_ratio_file,
+        exam_output_file,
+        exam_regression_file,
+        true,
+        1, // 交互深度
+        1, // 交互规模
+        0,
+        0
+    };
 
-    // 满足某个条件时进入扩张测试模式
-	auto test_result = parser_factory.expandExamModel(
-		exam_node,
-		exam_accuracy_file,
-		exam_answer_file,
-		exam_order_file,
-		exam_ratio_file,
-		exam_output_file,
-		exam_regression_file,
-		1 // 扩张程度为1
-	);
+    auto stable_sequences = parser_factory.SelfIteration(normal_mode, exam_mode);
+    for (auto s : stable_sequences)
+        db.Add(s);
 
-	parser_factory.classify(
-		test_result,
-		exam_node,
-		exam_accuracy_file,
-		exam_answer_file,
-		exam_order_file,
-		exam_ratio_file,
-		exam_output_file,
-		exam_regression_file
-		);
-	return 0;
+    return 0;
 }
