900723eb9e0634ff07697a395b6385adaabefeda
Nomel-Cool <619471786@qq.com>
2024-08-09 14:11:09 +0800

    添加形成序对比算法

--- parser.h
+++ parser.h
@@ -7,7 +7,10 @@
 #include <vector>
 #include <map>
 #include <string>
+
 #include <iostream>
+#include <utility>
+#include <algorithm>
 
 #include "dispatcher.h"
 #include "decision.h"
@@ -112,8 +115,64 @@ public:
 			}
 		}
 	}
+	std::vector<std::vector<std::pair<size_t, size_t>>> CollectRawSequences(interaction_param exam_mode)
+	{
+		std::vector<std::vector<std::pair<size_t, size_t>>> explicit_sequences;
+		while (exam_mode.interaction_depth--)
+		{
+			auto resultant_sequences = expandExamModel(
+				exam_mode.manipulate_node,
+				exam_mode.accuracy_file,
+				exam_mode.answer_file,
+				exam_mode.order_file,
+				exam_mode.ratio_file,
+				exam_mode.output_file,
+				exam_mode.regression_file,
+				exam_mode.interaction_scale, // 扩张程度为1
+				exam_mode.expand_nodes
+			);
+			// 顺手存储相邻去重后的序列
+			auto explicit_sequence = adjoinUnify(resultant_sequences);
+			explicit_sequences.emplace_back(std::move(explicit_sequence));
+		}
+		return explicit_sequences;
+	}
+	std::vector<std::vector<std::vector<bool>>> FindPattern(const std::vector<std::vector<std::pair<size_t, size_t>>>& simplified_sequences)
+	{
+		size_t num_samples = simplified_sequences.size();
+		size_t d = simplified_sequences[0].size();
+		std::vector<std::vector<std::vector<bool>>> dp(num_samples, std::vector<std::vector<bool>>(d, std::vector<bool>(d + 1, false)));
 
-
+		for (size_t length = 1; length <= d; ++length)
+		{
+			for (size_t start = 0; start <= d - length; ++start)
+			{
+				bool common = true;
+				for (size_t i = 1; i < num_samples; ++i)
+				{
+					bool match = false;
+					for (size_t j = 0; j <= d - length; ++j)
+					{
+						if (std::equal(simplified_sequences[0].begin() + start, simplified_sequences[0].begin() + start + length,
+							simplified_sequences[i].begin() + j))
+						{
+							match = true;
+							break;
+						}
+					}
+					if (!match)
+					{
+						common = false;
+						break;
+					}
+				}
+				if (common)
+					for (size_t i = 0; i < num_samples; ++i)
+						dp[i][start][length] = true;
+			}
+		}
+		return dp;
+	}
 protected:
 	SizeT7 Interaction(
 		const std::string& manipulate_node,
@@ -197,64 +256,7 @@ protected:
 		}
 		return feed_back; // 返回最后一次交互后的反馈
 	}
-	std::vector<std::pair<size_t, size_t>> expandExamModel(
-		const std::string& manipulate_node,
-		const std::string& exam_accuracy_file,
-		const std::string& exam_answer_file,
-		const std::string& exam_order_file,
-		const std::string& exam_ratio_file,
-		const std::string& exam_output_file,
-		const std::string& exam_regression_file,
-		const size_t& extra_size,
-		std::vector<std::vector<size_t>>& expand_nodes,
-		const size_t& convergence_limit = 5
-	)
-	{
-		// 挂起现场
-		m_dispatch.preserver(manipulate_node);
-		// 初始化实验环境
-		m_dispatch.examination(max_size, max_value, extra_size, expand_nodes); // max_size, max_value, extra_size
-		// 初始化抽象交互环境
-		m_decise.updateCurrentScale(expand_nodes.empty() ? extra_size : expand_nodes.size());
-		std::tuple<size_t, size_t> first_interactor = std::make_tuple(0, 0);
-		int counter = 0;
-		while (true)
-		{
-			auto feed_back = Interaction(
-				manipulate_node,
-				exam_accuracy_file,
-				exam_answer_file,
-				exam_order_file,
-				exam_ratio_file,
-				exam_output_file,
-				exam_regression_file,
-				true
-			);
 
-			if (std::get<0>(first_interactor) == std::get<1>(first_interactor))
-				first_interactor = std::make_tuple(std::get<1>(feed_back), std::get<2>(feed_back));
-
-			if ((std::get<0>(first_interactor) == std::get<1>(feed_back) && std::get<1>(first_interactor) == std::get<2>(feed_back)) ||
-				(std::get<0>(first_interactor) == std::get<2>(feed_back) && std::get<1>(first_interactor) == std::get<1>(feed_back)))
-				counter++;
-
-			if (counter == convergence_limit)
-			{
-				counter = 0;
-				break; // 当出现第2次实验头时，认为达到了平衡标准
-			}
-		}
-		// 到达认知收敛平衡标准后，获取virtuality记录的抽象形成序
-		auto sequences = m_dispatch.getInteractions(manipulate_node);
-
-		// 清楚抽象形成序记录缓存
-		m_dispatch.cleanCache(manipulate_node);
-
-		// 还原现场
-		m_dispatch.recover(manipulate_node);
-		m_decise.updateCurrentScale(expand_nodes.empty() ? -1 * extra_size : -1 * expand_nodes.size());
-		return sequences;
-	}
 	StableSequencesData lexicalization(
 		const std::vector<std::pair<size_t, size_t>>& sequences,
 		const std::string& manipulate_node,
@@ -321,6 +323,82 @@ protected:
 			else
 				break;
 		return static_cast<double>(match_length) / stable_length;
+	}
+	std::vector<std::pair<size_t, size_t>> expandExamModel(
+		const std::string& manipulate_node,
+		const std::string& exam_accuracy_file,
+		const std::string& exam_answer_file,
+		const std::string& exam_order_file,
+		const std::string& exam_ratio_file,
+		const std::string& exam_output_file,
+		const std::string& exam_regression_file,
+		const size_t& extra_size,
+		std::vector<std::vector<size_t>>& expand_nodes,
+		const size_t& convergence_limit = 5
+	)
+	{
+		// 挂起现场
+		m_dispatch.preserver(manipulate_node);
+		// 初始化实验环境
+		m_dispatch.examination(max_size, max_value, extra_size, expand_nodes); // max_size, max_value, extra_size
+		// 初始化抽象交互环境
+		m_decise.updateCurrentScale(expand_nodes.empty() ? extra_size : expand_nodes.size());
+		std::tuple<size_t, size_t> first_interactor = std::make_tuple(0, 0);
+		int counter = 0;
+		while (true)
+		{
+			auto feed_back = Interaction(
+				manipulate_node,
+				exam_accuracy_file,
+				exam_answer_file,
+				exam_order_file,
+				exam_ratio_file,
+				exam_output_file,
+				exam_regression_file,
+				true
+			);
+
+			if (std::get<0>(first_interactor) == std::get<1>(first_interactor))
+				first_interactor = std::make_tuple(std::get<1>(feed_back), std::get<2>(feed_back));
+
+			if ((std::get<0>(first_interactor) == std::get<1>(feed_back) && std::get<1>(first_interactor) == std::get<2>(feed_back)) ||
+				(std::get<0>(first_interactor) == std::get<2>(feed_back) && std::get<1>(first_interactor) == std::get<1>(feed_back)))
+				counter++;
+
+			if (counter == convergence_limit)
+			{
+				counter = 0;
+				break; // 当出现第2次实验头时，认为达到了平衡标准
+			}
+		}
+		// 到达认知收敛平衡标准后，获取virtuality记录的抽象形成序
+		auto sequences = m_dispatch.getInteractions(manipulate_node);
+
+		// 清楚抽象形成序记录缓存
+		m_dispatch.cleanCache(manipulate_node);
+
+		// 还原现场
+		m_dispatch.recover(manipulate_node);
+		m_decise.updateCurrentScale(expand_nodes.empty() ? -1 * extra_size : -1 * expand_nodes.size());
+		return sequences;
+	}
+	std::vector<std::pair<size_t, size_t>> adjoinUnify(std::vector<std::pair<size_t, size_t>> raw_sequences)
+	{
+		if (raw_sequences.empty()) return {};
+
+		std::vector<std::pair<size_t, size_t>> result;
+		result.push_back(raw_sequences[0]);
+
+		for (size_t i = 1; i < raw_sequences.size(); ++i) {
+			auto& last = result.back();
+			auto& current = raw_sequences[i];
+
+			if (!((last.first == current.first && last.second == current.second) ||
+				(last.first == current.second && last.second == current.first)))
+				result.push_back(current);
+		}
+
+		return result;
 	}
 
 private:
